677042009ac94f9dffaed3833d69bb8f7ebda682
infinityMark <109346958+infinityMark@users.noreply.github.com>
2023-09-20 13:21:07 +0800

    Create mark.js

---
+++ mark.js
@@ -0,0 +1,19 @@
+let Arry_mark=[1,2,3,4,5,6,7]
+let read = 250
+let writing = 200
+let listening = 300
+let SBA = 75
+let speaking = 75
+for(let i = 0;i<=Arry_mark.length;i++){
+    for(let x = 0;x<=Arry_mark.length;x++){
+        for(let z = 0;z<=Arry_mark.length;z++){
+            for(let y = 0;y<=Arry_mark.length;y++){
+                for(let u = 0;u<=Arry_mark.length;u++){
+                    if(read*Arry_mark[i]+writing*Arry_mark[x]+listening*Arry_mark[z]+SBA*Arry_mark[y]+speaking*Arry_mark[u]){
+                        
+                    }
+                }
+            }
+        }
+    }
+}
